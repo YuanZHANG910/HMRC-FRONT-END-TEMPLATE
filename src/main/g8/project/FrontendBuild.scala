40b3f5c1401b82b49c719eb2fc67607cf82f5b1b
Yuan.ZHANG <yuan.zhang@digital.hmrc.gov.uk>
2017-01-04 13:44:34 +0000

    use hmrc libarary

--- src/main/g8/project/FrontendBuild.scala
+++ src/main/g8/project/FrontendBuild.scala
@@ -15,17 +15,17 @@ object FrontendBuild extends Build with MicroService {
   val compile = Seq(
     ws,
     "com.typesafe.play" %% "play" % PlayVersion.current,
-    "uk.gov.hmrc" %% "frontend-bootstrap" % "6.7.0",
-    "uk.gov.hmrc" %% "play-partials" % "4.6.0",
-    "uk.gov.hmrc" %% "play-authorised-frontend" % "5.7.0",
-    "uk.gov.hmrc" %% "play-config" % "2.1.0",
-    "uk.gov.hmrc" %% "logback-json-logger" % "2.1.1",
-    "uk.gov.hmrc" %% "govuk-template" % "4.0.0",
-    "uk.gov.hmrc" %% "play-ui" % "4.17.2"
+    "uk.gov.hmrc" %% "frontend-bootstrap" % "7.10.0",
+    "uk.gov.hmrc" %% "play-partials" % "5.2.0",
+    "uk.gov.hmrc" %% "play-authorised-frontend" % "6.2.0",
+    "uk.gov.hmrc" %% "play-config" % "3.0.0",
+    "uk.gov.hmrc" %% "logback-json-logger" % "3.1.0",
+    "uk.gov.hmrc" %% "govuk-template" % "5.0.0",
+    "uk.gov.hmrc" %% "play-ui" % "5.3.0"
   )
 
   def test(scope: String = "test") = Seq(
-    "uk.gov.hmrc" %% "hmrctest" % "1.8.0" % scope,
+    "uk.gov.hmrc" %% "hmrctest" % "2.2.0" % scope,
     "org.scalatest" %% "scalatest" % "2.2.6" % scope,
     "org.pegdown" % "pegdown" % "1.6.0" % scope,
     "org.jsoup" % "jsoup" % "1.8.1" % scope,
